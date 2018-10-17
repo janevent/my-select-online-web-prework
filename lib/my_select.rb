4483fa289a2013695a920946fd508d4d104ad1b6
Jane Venter <janeanneventer@gmail.com>
2018-10-17 18:29:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while i < collection.length 
     collection[i]
     if yield(collection[i])
-     new_array << yield(collection[i])
+     new_array << collection[i]
     end
   i += 1
   end
