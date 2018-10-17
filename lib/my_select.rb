67f6b8616884d2da0dc101f10a03876b59dfb4a9
Jane Venter <janeanneventer@gmail.com>
2018-10-17 18:14:59 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,11 @@
 def my_select(collection)
  i = 0 
+ new_array = []
  while i < collection.length 
   collection[i]
+  if yield(collection[i]) == TRUE
+    new_array << yield(collection[i])
+  end
   i += 1
+  end
 end
